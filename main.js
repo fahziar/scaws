92942fd487616950f7e7a0300fe60182de25ee59
fahziar <fahziar_rw@yahoo.co.uk>
2015-10-26 12:33:36 +0700

    Integration

--- main.js
+++ main.js
@@ -18,102 +18,79 @@ var modelCreated = false;
 var Firebase = require('firebase');
 var FbUriFactory = require('./configuration/FirebaseUriFactory');
 
-//var date = new Date();
-//console.log(date);
-//times(10, 20, 23, 1, 2, 2000);
-//date = new Date();
-//console.log(date);
-//
-////var pin1 = new mraa.Aio(2);
-////function periodicActivity(){
-////    
-////    watering = threshold.checkThreshold();
-////    console.log(watering);
-////    setTimeout(periodicActivity, 500);
-////}
-//
-//var open = false;
-//var time = 1;
-//var moist = 0;
-//var classify = [];
-//var jam = 1;
-//function periodicActivity(){
-//    if(jam > 7){
-//        jam = 1;
-//    }
-//    console.log(jam);
-//    var i = 0;
-//    for (var key in valves){
-//        isWatering[i] = valves[key].check(jam);
-//        i++;
-//    }
-//    console.log(isWatering);
-//    //printSensor();
-//    getData();
-//    setModel();
-//    jam +=2;
-//    setTimeout(periodicActivity, 2000);
-//}
-//
-//function printSensor(){
-//    var soil1 = sensorInstances.soils['soil1'].getValue()/1023;
-//    var soil2 = sensorInstances.soils['soil2'].getValue()/1023;
-//    var soil3 = sensorInstances.soils['soil3'].getValue()/1023;
-//    var soil4 = (soil1 + soil2 + soil3)/3;
-//    var soil5 = (soil1 + soil2 + soil3)/3;
-//    var light = 1;
-//    var input =  [soil1, soil2, soil3, soil4, soil5, light];
-//    console.log(input);
-//}
-//
-//function getData(){
-//    var currentTime = (new Date()).getTime();
-//    if(currentTime-timetemp >=getDataDuration){
-//        var data = {};
-//        var soil1 = sensorInstances.soils['soil1'].getValue()/1023;
-//        var soil2 = sensorInstances.soils['soil2'].getValue()/1023;
-//        var soil3 = sensorInstances.soils['soil3'].getValue()/1023;
-//        var soil4 = (soil1 + soil2 + soil3)/3;
-//        var soil5 = (soil1 + soil2 + soil3)/3;
-//        var light = 1;
-//        data.input =  [soil1, soil2, soil3, soil4, soil5, light];
-//        data.output = isWatering;
-//        console.log(data)
-//        var watering = false;
-//        if(isWatering.indexOf(1) != -1){
-//            watering = true;
-//        }
-//        learning.addDataSet(data, watering);
-//        timetemp = currentTime;
-//    }
-//}
-//
-//function setModel(){
-//    var currentTime = new Date();
-//    if(currentTime.getHours == 23 && !modelCreated){
-//        console.log("Creating Model");
-//        learning.createModel();
-//        modelCreated = true;
-//        console.log("Creating Model: Done");
-//        timetemp = (new Date()).getTime();
-//    }
-//    else if(currentTime.getHours != 23 && modelCreated){
-//        modelCreated = false;
-//    }
-//}
-//
-//periodicActivity();
+var date = new Date();
+console.log(date);
+times(10, 20, 23, 1, 2, 2000);
+date = new Date();
+console.log(date);
+
+var open = false;
+var time = 1;
+var moist = 0;
+var classify = [];
+var jam = 1;
+function periodicActivity(){
+    //Kodenya agi
+    jam = (new Date()).getHours();
+    var i = 0;
+    for (var key in valves){
+        isWatering[i] = valves[key].check(jam);
+        i++;
+    }
+    console.log(isWatering);
+    getData();
+    setModel();
+
+    //Kodenya oji
+    new Firebase(FbUriFactory.getEndponint() + '/.info/connected').once('value', function(data){console.log('status: ' + data.val());});
+    firebase.pushSensorValues();
+    firebase.pushValveValues();
+    setTimeout(periodicActivity, 500);
+
+    setTimeout(periodicActivity, 2000);
+}
+
+function getData(){
+    var currentTime = (new Date()).getTime();
+    if(currentTime-timetemp >=getDataDuration){
+        var data = {};
+        var soil1 = sensorInstances.soils['soil1'].getValue()/1023;
+        var soil2 = sensorInstances.soils['soil2'].getValue()/1023;
+        var soil3 = sensorInstances.soils['soil3'].getValue()/1023;
+        var soil4 = sensorInstances.soils['soil4'].getValue()/1023;
+        var soil5 = sensorInstances.soils['soil5'].getValue()/1023;
+        var light = sensorInstances['light'].getValue();
+        data.input =  [soil1, soil2, soil3, soil4, soil5, light];
+        data.output = isWatering;
+        console.log(data);
+        var watering = false;
+        if(isWatering.indexOf(1) != -1){
+            watering = true;
+        }
+        learning.addDataSet(data, watering);
+        timetemp = currentTime;
+    }
+}
+
+function setModel(){
+    var currentTime = new Date();
+    if(currentTime.getHours == 23 && !modelCreated){
+        console.log("Creating Model");
+        learning.createModel();
+        modelCreated = true;
+        console.log("Creating Model: Done");
+        timetemp = (new Date()).getTime();
+    }
+    else if(currentTime.getHours != 23 && modelCreated){
+        modelCreated = false;
+    }
+}
+
 firebase.connectionManager();
 firebase.modeListener();
 firebase.initForceOpenListener();
 firebase.initSensorEnabledListener();
 firebase.initThresholdListener();
 firebase.initScheduleListener();
-function periodicActivity(){
-    new Firebase(FbUriFactory.getEndponint() + '/.info/connected').once('value', function(data){console.log('status: ' + data.val());});
-    firebase.pushSensorValues();
-    firebase.pushValveValues();
-    setTimeout(periodicActivity, 500);
-}
 
 periodicActivity();
